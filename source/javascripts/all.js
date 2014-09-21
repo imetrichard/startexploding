23b4c43439dd7754fcd4d1c2c7137c378a61c404
Richard Enlow <richard@amoe.ba>
2014-09-20 21:03:41 -0700

    Removing Greeting Script

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,22 +1,21 @@
-function show() {
-  var nowhrs = new Date().getHours();
-  var nowmins = new Date().getMinutes();
-  var hrsmins = nowhrs + (nowmins/60); // decimals of an hour
-
-
-  var schedule = [
-    ['Message 1',[9],[18]], // times are expressed as hours + decimals of an hour, e.g. 30 minutes is .5 hours.
-    ['Message 2',[18],[21.5]],
-    ['Message 3',[19.5],[20.25]]
-    ['Looks Like Youre Working Late',[20.5],[5]]
-  ]
-
-  var themessage = "";
-  for (var i =0; i <schedule.length; i++) {
-    if ((hrsmins >= schedule[i][1]) && (hrsmins < schedule[i][2])) {
-      themessage += schedule[i][0] + "<br>";
-      document.getElementById("greeting").innerHTML = themessage;
-    }
-  }
-
-}
+// Maybe I'll bring this feature back someday
+//function show() {
+//  var nowhrs = new Date().getHours();
+//  var nowmins = new Date().getMinutes();
+//  var hrsmins = nowhrs + (nowmins/60); // decimals of an hour
+//
+//
+//  var schedule = [
+//    ['Morning',[9],[18]], // times are expressed as hours + decimals of an hour, e.g. 30 minutes is .5 hours.
+//    ['Afternoon',[18],[21.5]],
+//    ['Evening',[19.5],[5]]
+//  ]
+//
+//  var themessage = "";
+//  for (var i =0; i <schedule.length; i++) {
+//    if ((hrsmins >= schedule[i][1]) && (hrsmins < schedule[i][2])) {
+//      themessage += "<h1>" +schedule[i][0] + "</h1><br>";
+//      document.getElementById("greeting").innerHTML = themessage;
+//    }
+//  }
+//}
