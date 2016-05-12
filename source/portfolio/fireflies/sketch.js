d0ee30ba3101ae1a30bf2aaf4c430ea467656a93
Richard Enlow <richard@amoe.ba>
2016-05-11 23:08:45 -0700

    adjusting fireflies

--- source/portfolio/fireflies/sketch.js
+++ source/portfolio/fireflies/sketch.js
@@ -2,20 +2,24 @@ var bMultiplier = 0.6;
 var bug = [];
 var tree;
 
+// preload music and assets
 function preload() {
   buzz = loadSound('assets/music.mp3');
   tree = loadImage("assets/tree.png");
 }
 
+
+// define # of walkers in a for loop and pushes them out
 function setup() {
   createCanvas(windowWidth,windowHeight);
-  for (var i=0; i<75; i++) {
+  for (var i=0; i<50; i++) {
     bug.push(new Walker());
   }
   noStroke();
   buzz.loop();
 }
 
+// Create the array of bugs
 function draw() {
   buzz.rate(0.5);
   background(25);
@@ -26,18 +30,21 @@ function draw() {
   image(tree, 0, 0,width,height);
 }
 
+// Individual bug
 function Walker() {
   var xoff = 1;
   var yoff = 1;
-  // this.diameter = random(10,20);
+  
   this.diameter = 20;
   this.x = noise(xoff) * width;
   this.y = noise(yoff) * height;
   this.pos = createVector (random(width/2-200,width/2+200),random(height/2-200,height/2+200));
+  this.size = createVector (random(5,20),random(5,20));
   this.vel = createVector(noise(xoff) * width,noise(yoff) * height);
   this.vel.mult(0.0001);
   this.col = random(0,255);
   this.where = this.pos.x + this.vel.x;
+  this.scaleFactor = random(0.2,1);
   
   var maxX = width - this.diameter/2-5;
   var maxY = height-height/4 - this.diameter/2-5;
@@ -60,23 +67,56 @@ function Walker() {
   this.acc.setMag(0.05);
   this.vel.add(this.acc);
   this.pos.add(this.vel);
-  // this.acc.setMag(random(0.001,0.4));
   }
   
   this.display = function () {
-    strokeWeight(random(30,40));
-    stroke(253, 241, 150,random(20,30));
-    fill(253, 241, 150,10);  
-    ellipse(this.pos.x+5, this.pos.y+15, 30, 30);
+    this.light();
+    this.wings();
+    this.body();
+  }
+  
+  this.body = function (){
     noStroke();
-    fill(random(225,255),50); 
-    ellipse(this.pos.x-random(0,5)+this.vel.x, this.pos.y+random(10,15)+this.vel.x, this.diameter-random(0,10), this.diameter);
-    ellipse(this.pos.x-random(0,5)+random(10,15)+this.vel.x, this.pos.y+15+random(this.vel.y,this.vel.y+2),this.diameter-random(0,10), this.diameter);
-    fill(253, 241, 150);   
-    ellipse(this.pos.x+random(3,5), this.pos.y+random(14,15), this.diameter-15, this.diameter-10);
+    if (this.scaleFactor <=0.3){
+      fill(254,251,175,100);   
+    } else if (this.scaleFactor <0.7 && this.scaleFactor >0.3){fill(254,251,175,150);}
+    else {fill(254,251,175);}
+    ellipse(this.pos.x+5, this.pos.y+15, this.diameter*this.scaleFactor-15*this.scaleFactor, this.diameter*this.scaleFactor-10*this.scaleFactor);
+  }
+  
+  this.wings = function (){
+    noStroke();
+    if (this.scaleFactor <=0.3){
+      fill(random(225,255),20);   
+    } else if (this.scaleFactor <0.7 && this.scaleFactor >0.3){fill(random(225,255),50);}
+    else {fill(random(225,255),70);}
+    
+    if (this.scaleFactor <0.3){
+      ellipse(this.pos.x+4, this.pos.y+16, this.diameter*this.scaleFactor-random(0,5), this.diameter*this.scaleFactor);
+      ellipse(this.pos.x+7, this.pos.y+16,this.diameter*this.scaleFactor-random(0,5), this.diameter*this.scaleFactor);
+    }
+    else if (this.scaleFactor <0.7){
+      ellipse(this.pos.x+2, this.pos.y+15+this.vel.x, this.diameter*this.scaleFactor-random(0,10)*this.scaleFactor, this.diameter*this.scaleFactor);
+      ellipse(this.pos.x+7, this.pos.y+15+random(this.vel.y,this.vel.y+2),this.diameter*this.scaleFactor-random(0,10)*this.scaleFactor, this.diameter*this.scaleFactor);
+    }
+    else {
+      ellipse(this.pos.x, this.pos.y+14+this.vel.x, this.diameter*this.scaleFactor-random(0,10)*this.scaleFactor, this.diameter*this.scaleFactor);
+      ellipse(this.pos.x+10, this.pos.y+14+random(this.vel.y,this.vel.y+2),this.diameter*this.scaleFactor-random(0,10)*this.scaleFactor, this.diameter*this.scaleFactor);
+    }
+  }
+
+  this.light = function (){
+    strokeWeight(random(30,40)*this.scaleFactor);
+    if (this.scaleFactor <=0.3){
+      stroke(253, 241, 150,random(0,10));  
+    } else if (this.scaleFactor <0.8 && this.scaleFactor >0.3){stroke(253, 241, 150,random(5,10));}
+    else {stroke(253, 241, 150,random(20,30));}
+    fill(253, 241, 150,0);  
+    ellipse(this.pos.x+5, this.pos.y+15, 20*this.scaleFactor, 20*this.scaleFactor);
   }
 }
 
+// Don't leave canvas
 function shouldMove(currentPosition, currentVelocity, min, maxX, maxY) {
   var nextX = currentPosition.x + currentVelocity.x;
   var nextY = currentPosition.y + currentVelocity.y;
@@ -87,6 +127,7 @@ function shouldMove(currentPosition, currentVelocity, min, maxX, maxY) {
   return true;
 }
 
+// Turns when hits wall
 function reverseVector (vector) {
   vector.x = vector.x * -1;
   vector.y = vector.y * -1;
